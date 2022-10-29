72fdd348be8930be98c5d33c9d9ee2ebfd0a2527
Lisa <79309884+nidvllrs@users.noreply.github.com>
2022-10-29 07:58:52 +0100

    Add files via upload

---
+++ Flood.py
@@ -0,0 +1,36 @@
+import fiona
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+from shapely.geometry import Point, Polygon, MultiPolygon, shape, LineString
+floods = gpd.read_file(r"C:\Users\ndvll\Desktop\data\Flood_Map_for_Planning_Rivers_and_Sea_Flood_Zone_3.shp")
+#print(floods.schema)
+stations = fiona.open(r"C:\Users\ndvll\Desktop\GB-railway-network-model\GB_rail_data\network\rail_nodes.shp")
+edges = fiona.open(r"C:\Users\ndvll\Desktop\GB-railway-network-model\GB_rail_data\network\rail_edges.shp")
+print(edges.schema)
+df = pd.read_csv(r"C:\Users\ndvll\Downloads\nodes_2.csv")
+#print(df.head())
+def coord_lister(geom):
+    coords = list(geom.exterior.coords)
+    return (coords)
+Multi = MultiPolygon([shape(pol['geometry']) for pol in fiona.open(r"C:\Users\ndvll\Desktop\data\Flood_Map_for_Planning_Rivers_and_Sea_Flood_Zone_3.shp")])
+Polygons = list(Multi.geoms)
+#print(Polygons)
+coordinates_list = floods.geometry.apply(coord_lister)
+stat_floods = []
+#print(coordinates_list)
+for x in range(len(df.index)):
+    print(x)
+    p=Point(df.iat[x,6],df.iat[x,7])
+    for y in Polygons:
+        if p.within(y) is True:
+            stat_floods.append(p)
+        else:
+            pass
+
+
+stat_floods_df = pd.DataFrame(stat_floods, columns=['points'])
+stat_floods_df.to_csv(r"C:\Users\ndvll\PycharmProjects\MDM3\Jacobs\stats_floods.csv", encoding='utf-8')
+
+#first = floods.next()
+#print(first) # (GeoJSON format)
---
+++ network.py
@@ -0,0 +1,30 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import momepy
+import contextily as cx
+from mpl_toolkits.basemap import Basemap
+rail = gpd.read_file(r"C:\Users\ndvll\OneDrive\Bureau\rail_network.csv")
+rail = rail.explode()
+#print(rail)
+#rail_df = gpd.GeoDataFrame(rail,geometry=WKT)
+
+rail.crs = "ETRS89"
+#print(rail['geometry'].crs)
+graph = momepy.gdf_to_nx(rail, approach='dual')
+#graph = momepy.node_degree(graph, name='degree')
+#nx.draw(graph, node_size=15)
+#rail.plot()
+df_wm = rail.to_crs(epsg=4258)
+#print(df_wm.columns)
+f, ax = plt.subplots(1, 3, figsize=(18, 6), sharex=True, sharey=True)
+rail.plot(color='#e32e00', ax=ax[0])
+for i, facet in enumerate(ax):
+    facet.set_title(("Streets", "Dual graph", "Overlay")[i])
+    facet.axis("off")
+nx.draw(graph, {n:[n[0], n[1]] for n in list(graph.nodes)}, ax=ax[1], node_size=15)
+rail.plot(color='#e32e00', ax=ax[2], zorder=-1)
+nx.draw(graph, {n:[n[0], n[1]] for n in list(graph.nodes)}, ax=ax[2], node_size=15)
+ax = df_wm.plot(figsize=(10, 10), alpha=0.5, edgecolor='k')
+cx.add_basemap(ax, crs=df_wm.crs, source=cx.providers.OpenStreetMap.Mapnik)
+plt.show()
