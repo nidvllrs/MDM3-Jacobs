b858f97d2d30b420f32deec5d51b4b38290dd018
tomrihoy <60923992+tomrihoy@users.noreply.github.com>
2022-11-07 13:04:14 +0000

    Update bristol_network.py
    Added weights to test centrality flow algorithm in Centrality.py

--- bristol_network.py
+++ bristol_network.py
@@ -20,11 +20,12 @@ Station_array=[S1,S2,S3,S4,S5,S6,S7,S8,S9,S10,S11,S12]
 
 G.add_nodes_from(Station_array)
 
+G.add_weighted_edges_from([(S3,S5,1),(S5,S12,1),(S12,S6,1),(S6,S8,1),(S8,S4,1),(S4,S9,1),(S9,S10,1),(S10,S1,1),(S1,S11,1),(S3,S2,1),(S2,S7,1),(S12,S13,1)])
 
-G.add_edges_from([(S3,S5),(S5,S12),(S12,S6),(S6,S8),(S8,S4),(S4,S9),(S9,S10),(S10,S1),(S1,S11),(S3,S2),(S2,S7),
-                  (S12,S13)])
-G.add_edges_from([(S5,S3),(S12,S5),(S6,S12),(S8,S6),(S4,S8),(S9,S4),(S10,S9),(S1,S10),(S11,S1),(S2,S3),(S7,S2),
-                  (S13,S12)])
+
+G.add_weighted_edges_from([(S5,S3,1),(S12,S5,1),(S6,S12,1),(S8,S6,1),(S4,S8,1),(S9,S4,1),(S10,S9,1),(S1,S10,1),(S11,S1,1),(S2,S3,1),(S7,S2,1),(S13,S12,1)])
+
+
 
 node_colour=['b','b','r','b','b','b','b','b','b','b','b','b']
 nx.draw(G, with_labels=True)
