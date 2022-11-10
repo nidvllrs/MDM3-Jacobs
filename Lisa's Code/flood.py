855ad6f4516f9f583429cb541abc1514ceb09916
Lisa <79309884+nidvllrs@users.noreply.github.com>
2022-11-10 11:04:08 +0000

    Update flood.py

--- Lisa's Code/flood.py
+++ Lisa's Code/flood.py
@@ -3,17 +3,19 @@ import pandas as pd
 import geopandas as gpd
 import numpy as np
 from shapely.geometry import Point, Polygon, MultiPolygon, shape, LineString
-floods = gpd.read_file(r"C:\Users\ndvll\Desktop\data\Flood_Map_for_Planning_Rivers_and_Sea_Flood_Zone_3.shp")
+
+floods = gpd.read_file("path/to/Flood_Map_for_Planning_Rivers_and_Sea_Flood_Zone_3.shp")
 #print(floods.schema)
-stations = fiona.open(r"C:\Users\ndvll\Desktop\GB-railway-network-model\GB_rail_data\network\rail_nodes.shp")
-edges = fiona.open(r"C:\Users\ndvll\Desktop\GB-railway-network-model\GB_rail_data\network\rail_edges.shp")
+stations = fiona.open("path/to/GB-railway-network-model/GB_rail_data/network/rail_nodes.shp")
+edges = fiona.open("path/to/GB-railway-network-model/GB_rail_data/network/rail_edges.shp")
 print(edges.schema)
-df = pd.read_csv(r"C:\Users\ndvll\Downloads\nodes_2.csv")
+
+df = pd.read_csv("path/to/nodes_2.csv")
 #print(df.head())
 def coord_lister(geom):
     coords = list(geom.exterior.coords)
     return (coords)
-Multi = MultiPolygon([shape(pol['geometry']) for pol in fiona.open(r"C:\Users\ndvll\Desktop\data\Flood_Map_for_Planning_Rivers_and_Sea_Flood_Zone_3.shp")])
+Multi = MultiPolygon([shape(pol['geometry']) for pol in fiona.open("path/to/data/Flood_Map_for_Planning_Rivers_and_Sea_Flood_Zone_3.shp")])
 Polygons = list(Multi.geoms)
 #print(Polygons)
 coordinates_list = floods.geometry.apply(coord_lister)
@@ -30,7 +32,7 @@ for x in range(len(df.index)):
 
 
 stat_floods_df = pd.DataFrame(stat_floods, columns=['points'])
-stat_floods_df.to_csv(r"C:\Users\ndvll\PycharmProjects\MDM3\Jacobs\stats_floods.csv", encoding='utf-8')
+stat_floods_df.to_csv("path/to/stats_floods.csv", encoding='utf-8')
 
 #first = floods.next()
 #print(first) # (GeoJSON format)
