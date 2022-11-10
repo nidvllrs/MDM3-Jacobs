5d73fdb84fa85f5c2f4af4ef8a31896ad681c776
Lisa <79309884+nidvllrs@users.noreply.github.com>
2022-11-10 10:53:57 +0000

    Create filter_od_matrix.py

---
+++ Lisa's Code/filter_od_matrix.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\ndvll\Desktop\GB-railway-network-model\GB_rail_data\outputs\od_matrix.csv")
+df2 = pd.read_csv(r"C:\Users\ndvll\OneDrive\Documents\bristol_ids.csv",sep=';')
+
+list = []
+
+for i in range(len(df2)):
+    print(i)
+    x = df2["node_id"][i]
+    for j in range(len(df)):
+        y = df["node_path"][j]
+        if x in y:
+            list.append(df.iloc[j])
+        else:
+            pass
+
+df3 = pd.DataFrame(list,columns=["number","origin_id","destination_id","node_path","edge_path","distance","journeys"]  )
+df3.to_csv(r"C:\Users\ndvll\bristol_od_matrix.csv")
